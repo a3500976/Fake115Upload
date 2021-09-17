7e43978c40d8439dbcb8952a05fc7c84d60bfe08
ZhangXiaoZhi <491434308@qq.com>
2021-09-17 16:48:18 +0800

    Create fake115upload.user.js
    fixed

--- fake115upload.user.js
+++ fake115upload.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @author       T3rry
+// @author       T3rry7f, zhang3
 // @name         115Link Helper
-// @description  115文件备份导入
-// @namespace    https://github.com/T3rry7f/Fake115Upload
-// @version      1.4.5
+// @description  115文件备份导入路人甲修复版
+// @namespace    https://github.com/zxzx19900109/Fake115Upload
+// @version      1.4.5.1
 // @match        https://115.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
@@ -12,10 +12,86 @@
 // @connect      webapi.115.com
 // @connect      115.com
 // @require      https://cdn.bootcss.com/jsSHA/2.3.1/sha1.js
-// @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=115012
 // @require      https://cdn.jsdelivr.net/npm/node-forge@0.10.0/dist/forge.min.js
 // @downloadURL none
 // ==/UserScript==
+
+function waitForKeyElements (
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
+
 (function() {
     'use strict';
 
